5f34d703a1c4725fe17b5d27522f442e8f46e011
Simon B. Gasse <sgasse@users.noreply.github.com>
2024-02-22 22:11:12 +0100

    Random planets

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -3,10 +3,15 @@ use bevy::{
     input::Input,
     prelude::*,
 };
+use bevy_prng::ChaCha8Rng;
+use bevy_rand::prelude::*;
+use rand::distributions::{Distribution, Standard, Uniform};
+use rand_core::RngCore;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(EntropyPlugin::<ChaCha8Rng>::default())
         .add_systems(Startup, setup_scene)
         .add_systems(Update, move_camera)
         .run();
@@ -16,6 +21,7 @@ fn setup_scene(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    mut rng: ResMut<GlobalEntropy<ChaCha8Rng>>,
 ) {
     commands.spawn((
         Camera3dBundle {
@@ -24,16 +30,26 @@ fn setup_scene(
                 ..default()
             },
             tonemapping: Tonemapping::TonyMcMapface,
-            transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+            transform: Transform::from_xyz(-120.0, -120., 20.0)
+                .looking_at(Vec3::new(0., 0., 0.), Vec3::Z),
             ..default()
         },
         BloomSettings::default(),
     ));
 
     let material_emissive1 = materials.add(StandardMaterial {
-        emissive: Color::rgb_linear(100., 100., 500.),
+        emissive: Color::rgb_linear(0., 0., 500.),
         ..default()
     });
+
+    let x_scale = 100.;
+    let y_scale = 100.;
+    let z_scale = 1.;
+
+    let x_ranger = Uniform::from(-1. * x_scale..1. * x_scale);
+    let y_ranger = Uniform::from(-1. * y_scale..1. * y_scale);
+    let z_ranger = Uniform::from(-1. * z_scale..1. * z_scale);
+    let radius_ranger = Uniform::from(0.5..1.0);
 
     let mesh = meshes.add(
         Mesh::try_from(shape::Icosphere {
@@ -43,15 +59,38 @@ fn setup_scene(
         .unwrap(),
     );
 
-    commands.spawn((
-        PbrBundle {
-            mesh: mesh.clone(),
-            material: material_emissive1,
-            transform: Transform::from_xyz(2.0, 0.0, 2.0),
-            ..default()
-        },
-        Planet,
-    ));
+    for _ in 0..50 {
+        let x = x_ranger.sample(rng.as_mut());
+        let y = y_ranger.sample(rng.as_mut());
+        let z = z_ranger.sample(rng.as_mut());
+        let radius = radius_ranger.sample(rng.as_mut());
+
+        commands.spawn((
+            PbrBundle {
+                mesh: meshes.add(
+                    Mesh::try_from(shape::Icosphere {
+                        radius,
+                        subdivisions: 5,
+                    })
+                    .unwrap(),
+                ),
+                material: material_emissive1.clone(),
+                transform: Transform::from_xyz(x, y, z),
+                ..default()
+            },
+            Planet,
+        ));
+    }
+
+    // commands.spawn((
+    //     PbrBundle {
+    //         mesh: mesh.clone(),
+    //         material: material_emissive1,
+    //         transform: Transform::from_xyz(2.0, 0.0, 2.0),
+    //         ..default()
+    //     },
+    //     Planet,
+    // ));
 }
 
 #[derive(Component)]
@@ -84,10 +123,10 @@ fn move_camera(
     }
 
     if keyboard.pressed(KeyCode::A) {
-        query_camera.get_single_mut().unwrap().translation.z += 0.1;
+        query_camera.get_single_mut().unwrap().translation.y += 0.1;
     }
 
     if keyboard.pressed(KeyCode::D) {
-        query_camera.get_single_mut().unwrap().translation.z -= 0.1;
+        query_camera.get_single_mut().unwrap().translation.y -= 0.1;
     }
 }
