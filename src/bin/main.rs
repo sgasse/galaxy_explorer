d19067dea14dd05bef170d9ca133c523bf2a206f
Simon B. Gasse <sgasse@users.noreply.github.com>
2024-02-22 22:11:12 +0100

    Larger star field

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -5,8 +5,8 @@ use bevy::{
 };
 use bevy_prng::ChaCha8Rng;
 use bevy_rand::prelude::*;
-use rand::distributions::{Distribution, Standard, Uniform};
-use rand_core::RngCore;
+use rand::distributions::{Distribution as _, Uniform};
+use rand_distr::Normal;
 
 fn main() {
     App::new()
@@ -17,12 +17,25 @@ fn main() {
         .run();
 }
 
+struct Planet {
+    x: f32,
+    y: f32,
+    z: f32,
+    radius: f32,
+}
+
 fn setup_scene(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
     mut rng: ResMut<GlobalEntropy<ChaCha8Rng>>,
 ) {
+    // Spiral
+    // x = r(phi)cos(phi)
+    // y = r(phi)sin(phi)
+    // logarithmic: r = a*exp(k*phi)
+    // hyperbolic spiral: r = a / phi
+
     commands.spawn((
         Camera3dBundle {
             camera: Camera {
@@ -30,7 +43,7 @@ fn setup_scene(
                 ..default()
             },
             tonemapping: Tonemapping::TonyMcMapface,
-            transform: Transform::from_xyz(-120.0, -120., 20.0)
+            transform: Transform::from_xyz(-120.0, -120., 60.0)
                 .looking_at(Vec3::new(0., 0., 0.), Vec3::Z),
             ..default()
         },
@@ -42,73 +55,18 @@ fn setup_scene(
         ..default()
     });
 
-    let x_scale = 100.;
-    let y_scale = 100.;
-    let z_scale = 1.;
-
-    let x_ranger = Uniform::from(-1. * x_scale..1. * x_scale);
-    let y_ranger = Uniform::from(-1. * y_scale..1. * y_scale);
-    let z_ranger = Uniform::from(-1. * z_scale..1. * z_scale);
-    let radius_ranger = Uniform::from(0.5..1.0);
-
-    let mesh = meshes.add(
-        Mesh::try_from(shape::Icosphere {
-            radius: 0.8,
-            subdivisions: 5,
-        })
-        .unwrap(),
-    );
-
-    for _ in 0..50 {
-        let x = x_ranger.sample(rng.as_mut());
-        let y = y_ranger.sample(rng.as_mut());
-        let z = z_ranger.sample(rng.as_mut());
-        let radius = radius_ranger.sample(rng.as_mut());
-
-        commands.spawn((
-            PbrBundle {
-                mesh: meshes.add(
-                    Mesh::try_from(shape::Icosphere {
-                        radius,
-                        subdivisions: 5,
-                    })
-                    .unwrap(),
-                ),
-                material: material_emissive1.clone(),
-                transform: Transform::from_xyz(x, y, z),
-                ..default()
-            },
-            Planet,
-        ));
+    for planet in rand_planet_field(500, &mut rng) {
+        spawn_planet(
+            &planet,
+            material_emissive1.clone(),
+            &mut commands,
+            &mut meshes,
+        );
     }
-
-    // commands.spawn((
-    //     PbrBundle {
-    //         mesh: mesh.clone(),
-    //         material: material_emissive1,
-    //         transform: Transform::from_xyz(2.0, 0.0, 2.0),
-    //         ..default()
-    //     },
-    //     Planet,
-    // ));
 }
 
 #[derive(Component)]
-struct Planet;
-
-fn debug_projection(query_camera: Query<&Projection, With<Camera>>) {
-    let projection = query_camera.single();
-    match projection {
-        Projection::Perspective(persp) => {
-            dbg!(persp);
-            // we have a perspective projection
-        }
-        Projection::Orthographic(ortho) => {
-            // we have an orthographic projection
-            dbg!(ortho);
-        }
-    }
-}
+struct PlanetAnchor;
 
 fn move_camera(
     mut query_camera: Query<&mut Transform, With<Camera>>,
@@ -129,4 +87,55 @@ fn move_camera(
     if keyboard.pressed(KeyCode::D) {
         query_camera.get_single_mut().unwrap().translation.y -= 0.1;
     }
+}
+
+fn spawn_planet(
+    planet: &Planet,
+    material: Handle<StandardMaterial>,
+    commands: &mut Commands,
+    meshes: &mut ResMut<Assets<Mesh>>,
+) {
+    let Planet { x, y, z, radius } = planet;
+
+    commands.spawn((
+        PbrBundle {
+            mesh: meshes.add(
+                Mesh::try_from(shape::Icosphere {
+                    radius: *radius,
+                    subdivisions: 5,
+                })
+                .unwrap(),
+            ),
+            material,
+            transform: Transform::from_xyz(*x, *y, *z),
+            ..default()
+        },
+        PlanetAnchor,
+    ));
+}
+
+fn rand_planet_field(n: usize, rng: &mut ResMut<GlobalEntropy<ChaCha8Rng>>) -> Vec<Planet> {
+    let rng = rng.as_mut();
+
+    let x_scale = 100.;
+    let y_scale = 100.;
+    let z_scale = 1.;
+
+    let x_ranger = Uniform::from(-1. * x_scale..1. * x_scale);
+    let y_ranger = Normal::new(0., y_scale / 2.).unwrap();
+    let z_ranger = Uniform::from(-1. * z_scale..1. * z_scale);
+    let radius_ranger = Normal::new(0.3, 0.1).unwrap().map(|val: f32| val.max(0.1));
+
+    let mut planets = Vec::with_capacity(n);
+
+    for _ in 0..n {
+        planets.push(Planet {
+            x: x_ranger.sample(rng),
+            y: y_ranger.sample(rng),
+            z: z_ranger.sample(rng),
+            radius: radius_ranger.sample(rng),
+        });
+    }
+
+    planets
 }
